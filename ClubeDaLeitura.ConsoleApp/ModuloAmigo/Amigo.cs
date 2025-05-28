dfae42b85ae0ed1bc6de9413d66b7e16d0f70565
Tiago <tiagoeweber@gmail.com>
2025-05-27 21:24:22 -0300

    adicionado o modulo amigo junto com as classes Amigo, RepositorioAmigo e TelaAmigo

---
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ClubeDaLeitura.ConsoleApp.ModuloAmigos
+{
+    public class Amigo
+    {
+        public string nome;
+        public string responsavel;
+        public string telefone;
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/TelaAmigo.cs
@@ -0,0 +1,7 @@
+﻿
+namespace ClubeDaLeitura.ConsoleApp.ModuloAmigos
+{
+    public class TelaAmigo
+    {
+    }
+}
