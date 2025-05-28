fd0b488976ff426a9ef8d6fa572a00ac4de6665d
Tiago <tiagoeweber@gmail.com>
2025-05-27 21:01:05 -0300

    Criado o ApresentarMenuPrincipal como um inicío ao projeto

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+      
+        }
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("|        Clube Da Leitura        |");
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Controle de Amigos");
+            Console.WriteLine("2 - Controle de Caixas");
+            Console.WriteLine("3 - Controle de Revistas");
+            Console.WriteLine("4 - Controle de Empréstimos");
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
         }
     }
 }
